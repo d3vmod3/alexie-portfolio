8d69a3f9cb021f577994c2bd29219699961f347c
Alexie Tuzon <astuzon@gmail.com>
2026-03-18 11:44:00 +0800

    added resume

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -56,6 +56,28 @@ const Navbar = () => {
           <Link className="hover:text-primary max-md:hidden" href="/contacts">
             Contacts
           </Link>
+          <Link
+            href="./Alexie_Tuzon_Resume.pdf"
+            target="_blank"
+            className="cursor-pointer flex justify-between items-end gap-2 hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black p-2 rounded-lg "
+          >
+            View Resume
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+          </Link>
+
           <ModeToggle />
         </div>
 
